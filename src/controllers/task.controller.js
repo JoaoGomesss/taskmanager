43698c99d0970338ae5c9e7f6f7f3a60dba7fa72
unknown <jowgomes3@gmail.com>
2024-08-13 18:20:23 -0300

    feat: move create task endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -6,7 +6,7 @@ class TaskController {
         this.res = res;
     }
 
-    async getTasks() {
+    async getAll() {
         try {
             const tasks = await TaskModel.find({});
             this.res.status(200).send(tasks);
@@ -15,7 +15,7 @@ class TaskController {
         }
     }
 
-    async getTasksById() {
+    async getById() {
         try {
             const taskId = this.req.params.id;
             const task = await TaskModel.findById(taskId);
@@ -27,6 +27,18 @@ class TaskController {
             }
 
             return this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async create() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+
+            await newTask.save();
+
+            this.res.status(201).send(newTask);
         } catch (error) {
             this.res.status(500).send(error.message);
         }
