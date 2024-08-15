8f2741c92d16f316fb0d33cecedcad02b08c3806
João Gomes <jowgomes3@gmail.com>
2024-08-15 08:45:17 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
 const taskRouter = require("./src/routes/task.routes");
 const connectToDatabase = require("./src/database/mongoose.database");
 
 dotenv.config();
 
 const app = express();
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+
+connectToDatabase();
 
 app.use("/tasks", taskRouter);
 
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}!!!`);
-});
-
-connectToDatabase();
+app.listen(8000, () => console.log(`Listening on port 8000!!!`));
