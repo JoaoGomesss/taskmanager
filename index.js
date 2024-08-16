2ee97d0bfd7530cff6e2f5c936e4cd92658486ac
João Gomes <jowgomes3@gmail.com>
2024-08-16 20:36:36 -0300

    fix: corsOptions

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: [
-        "https://taskmanager-vq5u.onrender.com",
-        "https://taskmanager-4x4.pages.dev",
-    ],
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "https://taskmanager-vq5u.onrender.com",
+            "https://taskmanager-4x4.pages.dev",
+        ];
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 connectToDatabase();
