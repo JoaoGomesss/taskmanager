1f059bdbf1ae42935ae052988513637360120a61
João Gomes <jowgomes3@gmail.com>
2024-08-16 20:04:14 -0300

    feat: add new origin on corsOptions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "https://taskmanager-vq5u.onrender.com",
     credentials: true,
     optionSuccessStatus: 200,
 };
